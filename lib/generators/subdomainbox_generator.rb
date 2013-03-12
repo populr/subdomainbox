a6930a51c6dee3e4533b60fe000918ea44b4c4f3
Daniel Nelson <dnelson@centresource.com>
2013-03-12 01:12:29 -0500

    shorter xsrf token secret

--- lib/generators/subdomainbox_generator.rb
+++ lib/generators/subdomainbox_generator.rb
@@ -1,7 +1,7 @@
 class SubdomainboxGenerator < Rails::Generators::Base
 
   def create_initializer_file
-    create_file "config/initializers/xsrf_token_secret.rb", "XSRF_TOKEN_SECRET = '#{SecureRandom.base64(64)}'"
+    create_file "config/initializers/xsrf_token_secret.rb", "XSRF_TOKEN_SECRET = '#{SecureRandom.base64(48)}'"
   end
 
 end
