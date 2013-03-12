0afc71f2d7e288569ea59481b82914d7a8e80436
Daniel Nelson <dnelson@centresource.com>
2013-03-12 01:10:05 -0500

    need quotes. shorter secret.

--- lib/generators/subdomainbox_generator.rb
+++ lib/generators/subdomainbox_generator.rb
@@ -1,7 +1,7 @@
 class SubdomainboxGenerator < Rails::Generators::Base
 
   def create_initializer_file
-    create_file "config/initializers/xsrf_token_secret.rb", "XSRF_TOKEN_SECRET = #{SecureRandom.base64(128)}"
+    create_file "config/initializers/xsrf_token_secret.rb", "XSRF_TOKEN_SECRET = '#{SecureRandom.base64(64)}'"
   end
 
 end
